4cd4df4916ea84e4768634170c5f8b5dc9131463
Sheldon Smickley <sheldon.smickley@gmail.com>
2024-10-13 15:08:38 -0400

    updated copy

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { CustomButton } from "@/components/ui/customButton";
-import { motion, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
 import { SelectBadge } from "@/components/ui/selectBadge";
 
 const Divider = () => {
@@ -30,39 +30,12 @@ const Divider = () => {
 
 const Hero = () => {
   const titleText = "The growth protocol";
-  const controls = useAnimation();
-
-  const titleContainer = {
-    hidden: { opacity: 0 },
-    visible: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.06, delayChildren: 0.02 * i },
-    }),
-  };
-
-  const titleChild = {
-    visible: {
-      opacity: 1,
-      x: 0,
-      y: 0,
-      transition: {
-        type: "spring",
-        damping: 12,
-        stiffness: 200,
-      },
-    },
-    hidden: {
-      opacity: 0,
-      x: -20,
-      y: 10,
-    },
-  };
 
   const subtitleContainer = {
     hidden: { opacity: 0 },
     visible: {
       opacity: 1,
-      transition: { staggerChildren: 0.1, delayChildren: 0.2 },
+      transition: { staggerChildren: 0.05 },
     },
   };
 
@@ -85,28 +58,14 @@ const Hero = () => {
   return (
     <div className="relative text-white min-h-[90vh] flex flex-col w-full items-center justify-between pt-14 px-4">
       <div className="text-center sm:px-6 lg:px-8 lg:pt-12 sm:pt-8 pt-0">
-        <motion.h1
-          className="sm:text-[56px] text-6xl lg:text-8xl font-semibold sm:mb-8 mb-4 font-sans leading-tight"
-          variants={titleContainer}
-          initial="hidden"
-          animate="visible"
-          onAnimationComplete={() => controls.start("visible")}
-        >
-          {titleText.split("").map((char, index) => (
-            <motion.span
-              key={index}
-              variants={titleChild}
-              style={{ display: "inline-block" }}
-            >
-              {char === " " ? "\u00A0" : char}
-            </motion.span>
-          ))}
-        </motion.h1>
+        <h1 className="sm:text-[56px] text-6xl lg:text-8xl font-semibold sm:mb-8 mb-4 font-sans leading-tight">
+          {titleText}
+        </h1>
         <motion.div
           className="md:text-2xl sm:text-lg text-base sm:mb-8 mb-6 text-gray-300 flex flex-wrap justify-center items-center"
           variants={subtitleContainer}
           initial="hidden"
-          animate={controls}
+          animate="visible"
         >
           <motion.span
             variants={subtitleChild}
@@ -131,12 +90,6 @@ const Hero = () => {
             className="whitespace-nowrap mr-2 mb-2"
           >
             <SelectBadge variant="red">any action</SelectBadge>
-          </motion.span>
-          <motion.span
-            variants={subtitleChild}
-            className="whitespace-nowrap mr-2 mb-2"
-          >
-            in seconds
           </motion.span>
         </motion.div>
         <CustomButton customVariant="big">Try out Torque</CustomButton>
